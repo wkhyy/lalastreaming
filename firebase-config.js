d75c230ea30ddc379c2fc036c69b18c31a69d06b
wkhyy <jogelxd12@gmail.com>
2026-06-08 14:14:56 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,14 +5,17 @@
 // 4) Activa Realtime Database en modo prueba para empezar.
 
 const firebaseConfig = {
-  apiKey: "PEGA_AQUI_TU_API_KEY",
-  authDomain: "PEGA_AQUI_TU_AUTH_DOMAIN",
-  databaseURL: "PEGA_AQUI_TU_DATABASE_URL",
-  projectId: "PEGA_AQUI_TU_PROJECT_ID",
-  storageBucket: "PEGA_AQUI_TU_STORAGE_BUCKET",
-  messagingSenderId: "PEGA_AQUI_TU_MESSAGING_SENDER_ID",
-  appId: "PEGA_AQUI_TU_APP_ID"
+  apiKey: "AIzaSyCfOKe_XDP5PgQPqGO-KvqdEuD4pnwEimI",
+  authDomain: "lalastreaming.firebaseapp.com",
+  databaseURL: "https://lalastreaming-default-rtdb.firebaseio.com",
+  projectId: "lalastreaming",
+  storageBucket: "lalastreaming.firebasestorage.app",
+  messagingSenderId: "868227118841",
+  appId: "1:868227118841:web:8d601d78b54070cfb25428",
+  measurementId: "G-FFG6GFYQXL"
 };
+
+const FIREBASE_STORE_PATH = "lalastreaming/storeData";
 
 // Ruta donde se guardarán los datos de tu tienda
 const FIREBASE_STORE_PATH = "lalastreaming/storeData";
